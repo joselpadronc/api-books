34cdfafea75a61763d8855504c196ec32c88cbc1
joselpadronc <padroncjosel@gmail.com>
2020-01-13 22:02:29 -0400

    Agregando metodos DELETE y PUT y  Search para obtener de a un solo libro

--- app.py
+++ app.py
@@ -10,14 +10,30 @@ def index():
 	return jsonify({"ping": "pong"})
 
 
+#Obtener la lista de libros
 @app.route('/api/books', methods=["GET"])
 def  get_books():
 	return jsonify({"message": "Lista de Libros guardados"}, {"books": books_saved})
 
 
+#Buscar libro
+@app.route('/api/books/<int:book_id>', methods=["GET"])
+def search_book(book_id):
+	book_found = [book for book in books_saved if book["id"] == book_id]
+	
+	if len(book_found) > 0:
+		return jsonify({"message": "Este es el libro que buscas"}, book_found)
+
+	else:
+		return jsonify({"message": "The book not found"})
+
+
+
+#Publicar nuevo libro
 @app.route('/api/books/', methods=["GET", "POST"])
 def add_book():
 	book_data = {
+		"id": request.json['id'],
 		"title": request.json['title'],
 		"description": request.json['description'],
 		"author": request.json['author'],
@@ -26,6 +42,38 @@ def add_book():
 	books_saved.append(book_data)
 
 	return jsonify({"message": "Lista de Libros guardados"}, {"books": books_saved})
+
+
+#Eliminar un libro
+@app.route('/api/books/<int:book_id>', methods=["GET", "DELETE"])
+def remove_book(book_id):
+	book_found = [book for book in books_saved if book["id"] == book_id]
+	
+	if len(book_found) > 0:
+		books_saved.remove(book_found[0])
+		return jsonify({"message": "The book is deleted"}, books_saved)
+
+	else:
+		return jsonify({"message": "The book not found"})
+
+
+#Actulizar o editar un libro
+@app.route('/api/books/<int:book_id>', methods=["GET", "PUT"])
+def update_book(book_id):
+	book_found = [book for book in books_saved if book["id"] == book_id]
+	
+	if len(book_found) > 0:
+		book_found[0]['title'] = request.json['title']
+		book_found[0]['description'] = request.json['description']
+		book_found[0]['author'] = request.json['author']
+		book_found[0]['launching'] = request.json['launching']
+
+		return jsonify({"message": "The book is updated"}, books_saved)
+
+	else:
+		return jsonify({"message": "The book not found"})
+
+
 
 if __name__ == "__main__":
 	app.run(debug=True, port=8080)
--- books.py
+++ books.py
@@ -1,17 +1,20 @@
 Books = [
 	{
+		"id": 1,
 		"title": "Python para todos",
 		"description": "Este es un curso para iniciarse en el mundo de Python",
 		"author": "Raul Gonzales Duque",
 		"launching": "2010.03.22"
 	},
 	{
+		"id": 2,
 		"title": "Django, Guia definitiva",
 		"description": "Este es un curso para iniciarse en el mundo de Python con Django",
 		"author": "Saul Garcia M",
 		"launching": "2010.04.22"
 	},
 	{
+		"id": 3,
 		"title": "Aprendizaje Flask",
 		"description": "Este es un curso para aprender e iniciarse en el mundo de Python Flask",
 		"author": "Colaboradores de Stack Overflow",
