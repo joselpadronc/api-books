c94794cd3af127e84711bcdac0cf57be74fabb25
FrFernandez <frfernandez1996@gmail.com>
2020-02-09 16:58:42 -0400

    refactor(app): Refactor Route and Models
    Refactor Route add catch errores and modify models for add funcions save and delete.

--- app.py
+++ app.py
@@ -9,8 +9,12 @@ dbdir = 'sqlite:///' + os.path.abspath(os.getcwd()) + '/db_books.db'
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = dbdir
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 JSONSerializable(app)
+
+with app.app_context():
+	db.init_app(app)
+	db.create_all()
 
 
 def error_handler(err, msg, detail=None):
@@ -38,9 +42,17 @@ class Books(db.Model):
 	description = db.Column(db.String(255))
 	author = db.Column(db.String(100))
 	launching = db.Column(db.String(100))
+	
+	
+	def save(self):
+		db.session.add(self)
+		db.session.commit()
+	
+	def delete(self):
+		db.session.delete(self)
+		db.session.commit()
 
-
-
+		
 @app.route('/')
 def index():
 	return jsonify({"ping": "pong"})
@@ -52,72 +64,40 @@ def  get_books():
 	try:
 		return jsonify(ok=True, items=Books.query.all())
         except Exception as e:
-		abort(500)
+		return abort(500, e)
 
 
 #Buscar libro
-@app.route('/api/books/<int:book_id>', methods=["GET"])
+@app.route('/api/books/id/<int:book_id>', methods=["GET"])
 def search_book(book_id):
-	
-	if request.method == 'GET':
-		book_found = Books.query.filter_by(id=book_id).first()
-
-		return jsonify(
-			{
-				"title":book_found.title,
-				"description":book_found.description,
-				"author":book_found.author,
-				"launching":book_found.launching
-			}
-		)
-	
-	else:
-		return jsonify({"message": "Book not found"})
-
+	try:
+		return jsonify(ok=True, item=Books.query.filter_by(id=book_id).first())
+	except Exception as e:
+		return abort(404, e)
 
 
 #Publicar nuevo libro
-@app.route('/api/books/', methods=["POST"])
+@app.route('/api/books', methods=["POST"])
 def add_book():
-
-	if request.method == 'POST':
+	try:
 		new_book = Books(
-			title				= request.json["title"],
-			description = request.json["description"],
+			title			= request.json["title"],
+			description 		= request.json["description"],
 			author			= request.json["author"],
 			launching		= request.json["launching"]
 		)
 	
-		db.session.add(new_book)
-		db.session.commit()
+		new_book.save()
 
 		return jsonify({"message": "The book is added"})
+	exect Exception as e:
+		return abort(400, e)
+
 	
-	else:
-		return jsonify({"message": "The book isn\'t added"})
-
-
-#Eliminar un libro
-@app.route('/api/books/<int:book_id>', methods=["DELETE"])
-def remove_book(book_id):
-
-	if request.method == 'POST':
-		book_found = Books.query.filter_by(id=book_id).first()
-
-		db.session.delete(book_found)
-		db.session.commit()
-
-		return jsonify({"message":"Book deleted"})
-	
-	else:
-		return jsonify({"message": "Book not found"})
-
-
 #Actulizar o editar un libro
-@app.route('/api/books/<int:book_id>', methods=["GET", "PUT"])
+@app.route('/api/books/<int:book_id>', methods=["PUT"])
 def update_book(book_id):
-
-	if request.method == 'PUT':
+	try:
 		book_found = Books.query.filter_by(id=book_id).first()
 	
 		book_found.title       = request.json["title"]
@@ -125,22 +105,25 @@ def update_book(book_id):
 		book_found.author      = request.json["author"]
 		book_found.launching   = request.json["launching"]
 
-		db.session.add(book_found)
-		db.session.commit()
+		book_found.save()
 
-		return jsonify(
-			{
-				"title": book_found.title,
-				"description": book_found.description,
-				"author": book_found.author,
-				"launching": book_found.launching
-			}
-		)
+		return jsonify(book_found)
+	except Exception as e:
+		return abort(404, e)
+
 	
-	else:
-		return jsonify({"message": "Book not found"})
+#Eliminar un libro
+@app.route('/api/books/<int:book_id>', methods=["DELETE"])
+def remove_book(book_id):
+	try:
+		book_found = Books.query.filter_by(id=book_id).first()
+		
+		book_found.delete()
+
+		return jsonify({"message":"Book deleted"})
+	except Exception as e:
+		return abort(404, e)
 
 
 if __name__ == "__main__":
-	db.create_all()
 	app.run(debug=True, port=8080)
